43233fba84d62c78908bf40a32a17a51fa7237b8
fausther <wilfrid.fausther@bforcecure.com>
2023-02-01 19:54:22 +0100

    site de commerce

---
+++ centry/Appli/app_admin/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import redirect, render
+from django.views.generic.edit import UpdateView,CreateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from Baracuda_Service.models import Article
+from Baracuda_Service.forms import ArticleForm
+
+def dashboard(request):
+    return render(request,'db.html')
+
+@login_required
+def user_articles(request):
+    has_perm =False
+    if request.user.has_perm("Baracuda_Service.delete_article"):
+        has_perm = True
+   
+    list_articles = Article.objects.filter(user=request.user)
+    return render(request,'my-articles.html',{'list_articles':list_articles,"has_perm":has_perm})
+
+class AddArticle(LoginRequiredMixin,CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name ="add-article.html"
+    success_url ="my-articles"
+
+    def form_valid(self,form):
+        form.instance.user=self.request.user
+        return super().form_valid(form)
+
+class UpdateArticle(LoginRequiredMixin,UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'app_admin/article_form.html'
+
+class DeleteArticle(DeleteView):
+    model = Article
+    success_url = "/my-admin/my-articles"
+
+    def dispatch(self, request, *args, **kwargs): 
+        if not request.user.has_perm('Baracuda_Service.delete_article'):
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+    
